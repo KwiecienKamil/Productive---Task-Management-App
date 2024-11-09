42ee5b83fe056fafb3f6f046ed7826bb6f594379
Kamil <kamilkwiecien9@gmail.com>
2024-11-09 15:34:04 +0100

    Full functional app

---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import axios from "axios";
+import Tasks from "../components/Tasks";
+import Rewards from "../components/Rewards";
+
+const Dashboard = () => {
+  const [streak, setStreak] = useState(0);
+  const [firstDoneDate, setFirstDoneDate] = useState<string | null>(null); // New state to store the first done date
+
+  const currentUser = localStorage.getItem("user");
+  const currentUserValue = currentUser ? JSON.parse(currentUser) : {};
+  const currentUserId = currentUserValue.id;
+
+  const currentDoneDates = localStorage.getItem("doneDates");
+  const currentDoneDatesValue = currentDoneDates
+    ? JSON.parse(currentDoneDates)
+    : [];
+
+  // Filter done dates to include only those with the matching user ID
+  const filteredDoneDates = currentDoneDatesValue.filter(
+    (date: any) => date.User_id === currentUserId
+  );
+
+  useEffect(() => {
+    const isDataLoading = localStorage.getItem("Loading");
+    axios
+      .post(`${import.meta.env.VITE_API_URL}/getUsersTasks`, {
+        User_id: currentUserId,
+      })
+      .then((res) => {
+        localStorage.setItem("tasks", JSON.stringify(res.data));
+        if (isDataLoading === "true") {
+          localStorage.setItem("Loading", "false");
+          window.location.reload();
+        }
+      });
+  }, [currentUserId]);
+
+  useEffect(() => {
+    calculateStreak(filteredDoneDates);
+  }, []);
+
+  // Helper function to clear the time from a date object
+  const clearTime = (date: Date) => {
+    return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+  };
+
+  // Function to calculate the streak from today backwards, stopping at any break
+  const calculateStreak = (
+    doneDates: { Task_doneDate: string; Task_id: number }[]
+  ) => {
+    if (!doneDates.length) {
+      setStreak(0);
+      return;
+    }
+
+    // Extract unique days from the dates, sort in descending order
+    const uniqueDays = Array.from(
+      new Set(
+        doneDates.map(
+          (task) =>
+            new Date(`20${task.Task_doneDate.split("-").reverse().join("-")}`)
+              .toISOString()
+              .split("T")[0]
+        )
+      )
+    ).sort((a, b) => new Date(b).getTime() - new Date(a).getTime());
+
+    let currentStreak = 0;
+    const today = clearTime(new Date());
+
+    for (let i = 0; i < uniqueDays.length; i++) {
+      const currentDay = clearTime(new Date(uniqueDays[i]));
+      const expectedDate = new Date(today);
+      expectedDate.setDate(today.getDate() - currentStreak);
+
+      // Check if the current day matches the expected date in the streak
+      if (currentDay.getTime() === expectedDate.getTime()) {
+        currentStreak++;
+      } else {
+        break;
+      }
+    }
+
+    setStreak(currentStreak);
+
+    // Set the first done date (earliest date in uniqueDays)
+    if (uniqueDays.length) {
+      setFirstDoneDate(uniqueDays[uniqueDays.length - 1]);
+    }
+  };
+
+  localStorage.setItem("streak", JSON.stringify(streak));
+  localStorage.setItem("firstDoneDate", firstDoneDate || "");
+
+  return (
+    <div className="h-screen px-[2px] vsm:px-[2rem] flex justify-between pt-8 font-inter text-black bg-gradient-to-r from-[#FBAB7E] to-[#F7CE68] overflow-hidden">
+      <div className="flex">
+        <Navbar />
+        <Tasks />
+      </div>
+      <Rewards />
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ server/server.js
@@ -0,0 +1,243 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors')
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "productivedb"
+})
+
+app.get('/', (req,res) => {
+    return res.json("From server")
+})
+
+app.get("/users", (req,res) => {
+    const sql = "SELECT * FROM users";
+    db.query(sql, (err,data) => {
+        if(err) return res.json(err);
+        return res.json(data)
+    })
+});
+
+app.get("/getDoneDates", (req,res) => {
+    const sql = "SELECT Tasks.Task_id, Tasks.User_id, doneDates.Task_doneDate FROM Tasks JOIN doneDates ON Tasks.Task_id = doneDates.Task_id";
+    db.query(sql, (err,data) => {
+        if(err) return res.json(err);
+        return res.json(data)
+    })
+})
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query('SELECT * FROM users WHERE username = ? AND password = ?',
+    [username, password], (err, result) => {
+        if (err) {
+            return res.send({ err: err });
+        }
+
+        if (result.length > 0) {
+            return res.send(result);
+        } else {
+            return res.send({ message: 'Wrong email/password' });
+        }
+    });
+});
+
+
+
+app.post('/getUsersTasks', (req, res) => {
+    const User_id = req.body.User_id;
+    
+
+    db.query('SELECT * FROM Tasks WHERE User_id = ?',
+    [User_id], (err, result) => {
+        if (err) {
+            return res.send({ err: err });
+        }
+
+        if (result.length > 0) {
+            return res.send(result);
+        } else {
+            return res.send({ message: 'Bad request' });
+        }
+    });
+});
+
+app.post('/addDoneDate', (req, res) => {
+    const Task_id = req.body.Task_id;
+    const TodaysDate = req.body.TodaysDate;
+    
+
+    db.query('INSERT INTO doneDates (Task_id, Task_doneDate) VALUES (?, ?)',
+    [Task_id, TodaysDate], (err, result) => {
+        if (err) {
+            return res.send({ err: err });
+        }
+
+        if (result.length > 0) {
+            return res.send(result);
+        } else {
+            return res.send({ message: 'Bad request' });
+        }
+    });
+});
+
+app.post('/deleteDoneDate', (req, res) => {
+    const Task_id = req.body.Task_id;
+    const TodaysDate = req.body.TodaysDate;
+    
+
+    db.query('DELETE FROM doneDates WHERE Task_id = ? AND Task_doneDate = ?',
+    [Task_id, TodaysDate], (err, result) => {
+        if (err) {
+            return res.send({ err: err });
+        }
+
+        if (result.length > 0) {
+            return res.send(result);
+        } else {
+            return res.send({ message: 'Bad request' });
+        }
+    });
+});
+
+app.post('/updateTask', (req, res) => {
+    const Task_id = req.body.Task_id;
+    const newTaskTitle = req.body.newTaskTitle;
+    
+
+    db.query('UPDATE Tasks SET Task_title = ? WHERE Task_id = ?',
+    [Task_id, newTaskTitle], (err, result) => {
+        if (err) {
+            return res.send({ err: err });
+        }
+
+        if (result.length > 0) {
+            return res.send(result);
+        } else {
+            return res.send({ message: 'Bad request' });
+        }
+    });
+});
+
+app.post('/removeAllDoneDates', (req, res) => {
+    const Task_id = req.body.Task_id;
+    
+
+    db.query('DELETE FROM doneDates WHERE Task_id = ?',
+    [Task_id], (err, result) => {
+        if (err) {
+            return res.send({ err: err });
+        }
+
+        if (result.length > 0) {
+            return res.send(result);
+        } else {
+            return res.send({ message: 'Bad request' });
+        }
+    });
+});
+
+app.post('/removeTask', (req, res) => {
+    const Task_id = req.body.Task_id;
+    
+
+    db.query('DELETE FROM Tasks WHERE Task_id = ?',
+    [Task_id], (err, result) => {
+        if (err) {
+            return res.send({ err: err });
+        }
+
+        if (result.length > 0) {
+            return res.send(result);
+        } else {
+            return res.send({ message: 'Bad request' });
+        }
+    });
+});
+
+app.post('/tasks', (req, res) => {
+    const taskName = req.body.taskName;
+    const parsedUserId = req.body.parsedUserId;
+
+    db.query('INSERT INTO Tasks (Task_title, User_id) VALUES (?, ?)',
+    [taskName, parsedUserId], (err, result) => {
+        if (err) {
+            return res.send({ err: err });
+        }
+
+        if (result.length > 0) {
+            return res.send(result);
+        } else {
+            return res.send({ message: 'Wrong email/password' });
+        }
+    });
+});
+
+app.post('/getMissions', (req, res) => {
+    const User_id = req.body.User_id;
+    
+    db.query('SELECT missionName, missionValue FROM missions WHERE User_id = ?',
+    [User_id], (err, result) => {
+        if (err) {
+            return res.send({ err: err });
+        }
+
+        if (result.length > 0) {
+            return res.send(result);
+        } else {
+            return res.send({ message: 'Bad request' });
+        }
+    });
+});
+
+app.post('/addMission', (req, res) => {
+    const User_id = req.body.User_id;
+    const missionName = req.body.missionName;
+    const missionValue = req.body.missionValue;
+    
+    db.query('INSERT INTO missions (missionName, missionValue, User_id) VALUES (?, ?, ?)',
+    [missionName, missionValue, User_id ], (err, result) => {
+        if (err) {
+            return res.send({ err: err });
+        }
+
+        if (result.length > 0) {
+            return res.send(result);
+        } else {
+            return res.send({ message: 'Bad request' });
+        }
+    });
+});
+
+app.post('/claimReward', (req, res) => {
+    const Task_id = req.body.Task_id;
+    const TodaysDate = req.body.TodaysDate;
+    
+    db.query('INSERT INTO doneDates (Task_id, Task_doneDate) VALUES (?, ?)',
+    [Task_id, TodaysDate], (err, result) => {
+        if (err) {
+            return res.send({ err: err });
+        }
+
+        if (result.length > 0) {
+            return res.send(result);
+        } else {
+            return res.send({ message: 'Bad request' });
+        }
+    });
+});
+
+
+app.listen(8081, () => {
+    console.log('listening')
+})
