365074c0cc7d75c2a6cc158039c35dc6bce36e52
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2025-02-26 21:08:29 +0100

    Testing Login form

--- client/src/tests/Login.test.tsx
+++ client/src/tests/Login.test.tsx
@@ -1,12 +1,31 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
 import Login from "../pages/Login";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
 import "@testing-library/jest-dom/vitest";
 
-describe("Login", () => {
-  it("Renders login text", () => {
+vi.mock("../components/LoginForm", () => ({
+  default: () => <div data-testid="login-form">Mocked LoginForm</div>,
+}));
+
+describe("Login Page", () => {
+  beforeEach(() => {
+    global.fetch = vi.fn();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+    cleanup();
+  });
+
+  it("renders login page correctly", () => {
     render(<Login />);
-    const loginTexts = screen.getAllByText("Login");
+
+    const loginTexts = screen.getAllByText(/login/i);
     expect(loginTexts.length).toBeGreaterThan(0);
+
+    expect(screen.getByText(/welcome to/i)).toBeInTheDocument();
+    expect(screen.getByText(/productive/i)).toBeInTheDocument();
+
+    expect(screen.getByTestId("login-form")).toBeInTheDocument();
   });
 });
