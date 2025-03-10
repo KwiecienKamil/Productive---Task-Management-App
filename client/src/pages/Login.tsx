378390e8ad8a53225f73a4bca3e92024fefc50ea
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2025-03-10 19:28:33 +0100

    Login page changes

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -6,6 +6,11 @@ const Login = () => {
       data-theme="emerald"
       className="h-screen w-full flex-col flex items-center justify-center bg-pri px-4 py-2"
     >
+      <div className="absolute text-center top-24">
+        <p>Test user</p>
+        <p>Login: Kamil</p>
+        <p>Password: 12345</p>
+      </div>
       <p className="text-2xl pb-2 text-center text-black">
         Welcome to <span className="font-semibold">Productive</span>
       </p>
